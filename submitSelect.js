5f3476f8876ab96b97580085c3b56b9683e9b481
Tony <tony@thewholeworldwindow.co.uk>
2014-03-29 18:03:00 +0000

    First commit

---
+++ submitSelect.js
@@ -0,0 +1,29 @@
+// =================================================================================
+// Decide which submit button should be used when the return ket is pressed
+// =================================================================================
+
+function submitSelect() {
+
+	var form = $( 'form' );
+
+	form.on( 'keypress', function( event ) {
+		var tgt = $( event.target );
+
+		// Return if element in focus is a textarea
+		if( event.target.nodeName === 'TEXTAREA' ) return;
+
+		// Return if anything but the return key is hit
+		if ( event.which !== 13 ) return;
+
+		if ( tgt.attr( 'data-submit-with' ) !== undefined ) {
+			var buttonAttr = tgt.attr( 'data-submit-with' );
+
+			form.find( 'input[data-btn=' + buttonAttr + ']' ).trigger( 'click' );
+		}
+
+		if ( $( event.target ).attr( 'type' ) !== 'submit' ) {
+			event.preventDefault();
+			$( event.target ).trigger( 'click' );
+		}
+	});
+}
